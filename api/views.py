b64e1981555f5107da2c31d55b944bbe3e17f8bd
Vassili <vassili@tftak.eu>
2021-02-28 11:57:18 +0200

    add allowed hosts all and gitignore

--- api/views.py
+++ api/views.py
@@ -3,18 +3,19 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from api.FinanceService import asset_tracker
 from api.models import Transaction
 from api.serializers import TransactionSerializer
-from api.FinanceService import asset_tracker
 
 
 class RegistrationApiView(APIView):
 
-    def post(self, request):
-        data = request.data
-        User.objects.create_user(data['username'],
-                                 data['email'],
-                                 data['password'])
+    def post(self):
+        data = self.request.data
+        User.objects.create_user(data.get('username'),
+                                 data.get('email'),
+                                 data.get('password'))
         return Response('Registration successful!')
 
 
@@ -32,9 +33,10 @@ class TransactionApiView(ListCreateAPIView):
         dot = data.get('date_of_transfer')
         amount_crypto = data.get('amount_crypto')
         amount_cash = data.get('amount_cash')
-        print(type(amount_cash))
+
         if not amount_crypto:
             amount_crypto = float(amount_cash) / asset_tracker.get_asset_price_at_timepoint(asset, dot)
+
         serializer.save(amount_crypto=amount_crypto, user=self.request.user)
 
 
--- stonksBackend/settings.py
+++ stonksBackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'us+i0wo)2eae91hvq&8vy9c*7fz)_ugcd7$db44kbd3tsbth^b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
